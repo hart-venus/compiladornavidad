f3fe1ca91a864ce1283719572928fbd64a423251
Ariel Leyva <aleyva1509@gmail.com>
2023-12-08 19:48:41 -0600

    add: linting fix

--- src/main/java/com/wanderariel/App.java
+++ src/main/java/com/wanderariel/App.java
@@ -1,31 +1,34 @@
 package com.wanderariel;
-import java.io.Reader;
+
 import java.io.StringReader;
 import java_cup.runtime.Symbol;
 
 /**
- * Hello world!
+ * Hello world.
  *
  */
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        // new string reader
-        Lexer lexer = new Lexer(new StringReader("1 / 2 // 3 ** 5 /_ ! _/ * 10 + -3.7 - 3 \"Mario barboza \\n\" + 1 @ Hola, soy un comentario.")); 
-        Symbol token; 
-        try {
-            while ((token = lexer.next_token()).sym != sym.EOF) {
-                System.out.println(tokenToString(token));
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+public class App {
+  /**
+   * Clase principal que agarra el Lexer previamente generado y lo prueba con
+   * la entrada del archivo de prueba.
+   */
+  public static void main(String[] args) {
+    // new string reader
+    Lexer lexer = new Lexer(new StringReader("1 / 2 // 3 ** 5 ")); 
+    Symbol token; 
+    try {
+      while ((token = lexer.next_token()).sym != sym.EOF) {
+        System.out.println(tokenToString(token));
+      }
+    } catch (Exception e) {
+      System.out.println(e);
     }
+  }
 
-    private static String tokenToString(Symbol token) {
-        return sym.terminalNames[token.sym] + " " + token.value + " Line: " + token.left + " Column: " + token.right + "\n";
+  private static String tokenToString(Symbol token) {
+    return sym.terminalNames[token.sym] + " " + token.value + " Line: " 
+      + token.left + " Column: " + token.right + "\n";
 
-    }
+  }
 }
