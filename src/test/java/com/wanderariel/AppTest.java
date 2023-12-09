fecdb81f43b60fab283dfc5c90868a570e529a99
TuloShark <wander.tulo.jimenez@gmail.com>
2023-12-08 20:08:58 -0600

    Testing

--- src/test/java/com/wanderariel/AppTest.java
+++ src/test/java/com/wanderariel/AppTest.java
@@ -1,19 +1,12 @@
 package com.wanderariel;
 
-
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java_cup.runtime.Symbol;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+// AppTest.java
+public class AppTest {
     @Test
     public void testCupDependency() {
         Symbol symbol = new Symbol(0);
