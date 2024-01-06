7194734ee185432faa07cfa5f0549d6d347c19b6
TuloShark <wander.tulo.jimenez@gmail.com>
2024-01-06 15:08:20 -0600

    Symbol comparison

--- src/test/java/com/wanderariel/LexerTest.java
+++ src/test/java/com/wanderariel/LexerTest.java
@@ -4,10 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import java_cup.runtime.Symbol;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Clase de pruebas unitarias.
@@ -17,15 +20,29 @@ import static org.junit.Assert.assertTrue;
  * Salida: ninguna.
  * 
  */
-public class LexerTest {
 
-  @Test 
+public class LexerTest {
+  @Test
   public void testIntLiterals() throws FileNotFoundException, IOException {
     Lexer lexer = new Lexer(new BufferedReader(new FileReader("src/main/test/int_literals.txt")));
-    List<Symbol> expectedSymbols = lexer.getTokens();
-    // print expected symbols
-    System.out.println("Expected symbols:");
-    assertTrue(expectedSymbols.size() == 4);
+    List<Symbol> actualSymbols = lexer.getTokens();
 
+    // Formato brindado
+    List<String> actualTokenIdentifiers = actualSymbols.stream()
+        .map(symbol -> "#" + symbol.sym)
+        .collect(Collectors.toList());
+
+    // Basado en el contenido de int_literals.txt
+    List<String> expectedTokenIdentifiers = Arrays.asList("#26", "#26", "#2", "#26");
+
+    // Print actuales y esperados
+    System.out.println("Actual token identifiers:");
+    actualTokenIdentifiers.forEach(System.out::println);
+
+    System.out.println("Expected token identifiers:");
+    expectedTokenIdentifiers.forEach(System.out::println);
+
+    // Compare actuales y esperados
+    assertEquals(expectedTokenIdentifiers, actualTokenIdentifiers);
   }
 }
